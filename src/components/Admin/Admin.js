5afb05f2940918c102e439a69d103b72ded25ca8
Tyler <tdthompson@aep.com>
2019-02-11 12:31:10 -0500

    finished admin page, configured to show all users and their username, email, and uid key from firebase.

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withFirebase } from '../Firebase/Firebase-index';
 
-const Admin = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+class Admin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key
+      }));
+
+      this.setState({
+        users: usersList,
+        loading: false
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <div>
+        <h1>Admin</h1>
+        {loading && <div>Loading ...</div>}
+
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.uid}>
+        <div>
+          <strong>ID:</strong> {user.uid}
+        </div>
+        <div>
+          <strong>Email:</strong> {user.email}
+        </div>
+        <div>
+          <strong>Username:</strong> {user.username}
+        </div>
+      </li>
+    ))}
+  </ul>
 );
 
-export default Admin;
+export default withFirebase(Admin);
